d28f39c39ec5de690a822356cc17dedcdd84121d
Tom Anderson <toanderson@pivotal.io>
2015-03-27 11:22:59 +0000

    Parse URIs from credentials in VCAP_SERVICES

--- src/main/java/io/pivotal/labs/cfenv/CloudFoundryEnvironment.java
+++ src/main/java/io/pivotal/labs/cfenv/CloudFoundryEnvironment.java
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
-import java.util.HashSet;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class CloudFoundryEnvironment {
@@ -12,10 +15,9 @@ public class CloudFoundryEnvironment {
     public static final String VCAP_SERVICES = "VCAP_SERVICES";
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-    private final Set<String> serviceNames = new HashSet<>();
+    private final Map<String, String> serviceUris = new HashMap<>();
 
     public CloudFoundryEnvironment(Environment environment) throws CloudFoundryEnvironmentException {
-
         String vcapServices = environment.lookup(VCAP_SERVICES);
 
         JsonNode rootNode = parse(vcapServices);
@@ -23,13 +25,12 @@ public class CloudFoundryEnvironment {
         rootNode.forEach(serviceTypeNode -> {
             serviceTypeNode.forEach(serviceInstanceNode -> {
                 String name = serviceInstanceNode.get("name").asText();
-                serviceNames.add(name);
+                JsonNode uriNode = serviceInstanceNode.get("credentials").get("uri");
+                if (uriNode != null) {
+                    serviceUris.put(name, uriNode.asText());
+                }
             });
         });
-    }
-
-    public Set<String> getServiceNames() {
-        return serviceNames;
     }
 
     private JsonNode parse(String json) throws CloudFoundryEnvironmentException {
@@ -38,6 +39,14 @@ public class CloudFoundryEnvironment {
         } catch (IOException e) {
             throw new CloudFoundryEnvironmentException("error parsing JSON: " + json, e);
         }
+    }
+
+    public Set<String> getServiceNames() {
+        return serviceUris.keySet();
+    }
+
+    public URI getUri(String serviceName) throws URISyntaxException {
+        return new URI(serviceUris.get(serviceName));
     }
 
 }
--- src/test/java/io/pivotal/labs/cfenv/CloudFoundryEnvironmentTests.java
+++ src/test/java/io/pivotal/labs/cfenv/CloudFoundryEnvironmentTests.java
@@ -2,12 +2,17 @@ package io.pivotal.labs.cfenv;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
 
 public class CloudFoundryEnvironmentTests {
 
@@ -29,23 +34,62 @@ public class CloudFoundryEnvironmentTests {
     }
 
     @Test
-    public void shouldParseASystemService() throws Exception {
-        String json = ResourceUtils.loadResource("system_service.json");
-
-        CloudFoundryEnvironment environment = new CloudFoundryEnvironment(environment("VCAP_SERVICES", json));
+    public void shouldDetectASystemService() throws Exception {
+        CloudFoundryEnvironment environment = new CloudFoundryEnvironment(environmentWithVcapServices("system_service.json"));
         Set<String> serviceNames = environment.getServiceNames();
 
         assertThat(serviceNames, contains("myapp-db"));
     }
 
     @Test
-    public void shouldParseAUserProvidedService() throws Exception {
-        String json = ResourceUtils.loadResource("user_provided_service.json");
-
-        CloudFoundryEnvironment environment = new CloudFoundryEnvironment(environment("VCAP_SERVICES", json));
+    public void shouldDetectAUserProvidedService() throws Exception {
+        CloudFoundryEnvironment environment = new CloudFoundryEnvironment(environmentWithVcapServices("user_provided_service.json"));
         Set<String> serviceNames = environment.getServiceNames();
 
         assertThat(serviceNames, contains("search-engine"));
+    }
+
+    @Test
+    public void shouldParseTheUriFromASystemService() throws Exception {
+        CloudFoundryEnvironment environment = new CloudFoundryEnvironment(environmentWithVcapServices("system_service.json"));
+        URI uri = environment.getUri("myapp-db");
+
+        assertThat(uri, equalTo(URI.create("postgres://dxktcwjm:xxxxxxxx@babar.elephantsql.com:5432/dxktcwjm")));
+    }
+
+    @Test
+    public void shouldParseTheUriFromAUserProvidedService() throws Exception {
+        CloudFoundryEnvironment environment = new CloudFoundryEnvironment(environmentWithVcapServices("user_provided_service.json"));
+        URI uri = environment.getUri("search-engine");
+
+        assertThat(uri, equalTo(URI.create("https://duckduckgo.com/")));
+    }
+
+    @Test
+    public void shouldTolerateAServiceWithNoUri() throws Exception {
+        new CloudFoundryEnvironment(environmentWithVcapServices("syslog.json"));
+    }
+
+    @Test
+    public void shouldTolerateAServiceWithAMalformedUri() throws Exception {
+        new CloudFoundryEnvironment(environmentWithVcapServices("system_service.json", json -> json.replace("postgres://", "postgres:||")));
+    }
+
+    @Test(expected = URISyntaxException.class)
+    public void shouldNotRevealAMalformedUri() throws Exception {
+        CloudFoundryEnvironment environment = new CloudFoundryEnvironment(environmentWithVcapServices("system_service.json", json -> json.replace("postgres://", "postgres:||")));
+
+        environment.getUri("myapp-db");
+    }
+
+    private Environment environmentWithVcapServices(String jsonResourceName) throws IOException {
+        return environmentWithVcapServices(jsonResourceName, Function.identity());
+    }
+
+    private Environment environmentWithVcapServices(String jsonResourceName, Function<String, String> tweak) throws IOException {
+        String json = ResourceUtils.loadResource(jsonResourceName);
+        String tweakedJson = tweak.apply(json);
+        return environment("VCAP_SERVICES", tweakedJson);
     }
 
     private Environment environment(String name, String value) {
